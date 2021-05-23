9e443eb2d0666e82a153c4fd98ba6d14010fe0c8
Dan Gamble <dan@dangamble.co.uk>
2021-05-23 10:24:13 +0100

    Test getting the parent repo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const destinationOrganizationName = core.getInput('destination-organization-name')
 const destinationRepositoryName = core.getInput('destination-repository-name')
 const targetBranch = core.getInput('target-branch')
 
@@ -8,9 +9,18 @@ console.log(destinationRepositoryName, targetBranch)
 console.log(process.env)
 
 try {
+  main()
+} catch (error) {
+  core.setFailed(error.message)
+}
+
+async function main () {
   const octokit = github.getOctokit(process.env.BAO_API_TOKEN)
 
-  console.log(octokit)
+  console.log(await octokit.repos.listBranches({
+    owner: destinationOrganizationName,
+    repo: destinationRepositoryName,
+  }))
 
   // `who-to-greet` input defined in action metadata file
   console.log(`Hello ${destinationRepositoryName}!`)
@@ -21,6 +31,4 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
 }

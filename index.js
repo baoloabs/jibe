689cdeb6c46787a298a4f99d55e9d04e95cfff7c
Dan Gamble <dan@dangamble.co.uk>
2021-05-23 09:58:36 +0100

    Test commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const BAO_API_TOKEN = core.getInput('BAO_API_TOKEN')
+const destinationRepositoryName = core.getInput('destination-repository-name')
+const targetBranch = core.getInput('target-branch')
+
 try {
+  const octokit = github.getOctokit(BAO_API_TOKEN)
+
+  console.log(octokit, destinationRepositoryName, targetBranch)
+
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
+  console.log(`Hello ${destinationRepositoryName}!`)
 
   const time = (new Date()).toTimeString()
   core.setOutput('time', time)
